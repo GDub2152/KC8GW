03fa8e84d635508f988cc2c158acf10efbc42950
GDub2152 <107120490+GDub2152@users.noreply.github.com>
2026-07-28 20:57:43 -0400

    2.1

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kc8gw-v10-liveatc';
+const CACHE='kc8gw-v11-g1000-counter';
 const CORE=['./','index.html','qrz.html','liveatc.html','propagation.html','weather.html','adsb.html','media.html','projects.html','shack.html','contact.html','assets/css/style.css','assets/js/app.js','assets/js/config.js','assets/js/solar.js','assets/js/weather.js','assets/images/kc8gw-patch.png','assets/images/oshkosh-airshow-bg.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
